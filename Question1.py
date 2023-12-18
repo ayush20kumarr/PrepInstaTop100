78abdc8a3d8d4071ba0759bc80e74dec2c4c945a
Ayush Kumar <92459136+ayush20kumarr@users.noreply.github.com>
2023-12-18 15:14:46 -0400

    Add files via upload

---
+++ Question1.py
@@ -0,0 +1,16 @@
+# Check if a Number is Positive and Negative in Python
+
+# Given an integer input, The objective is to write a code to Check if a Number is Positive or Negative in C Language.
+
+# For Instance,
+# Input : num = 20
+# Output : The number is Positive.
+
+num = int(input("Enter a number: "))
+
+if num > 1:
+    print("Positive")
+elif num == 0:
+    print("Zero")
+else:
+    print("Negative")
